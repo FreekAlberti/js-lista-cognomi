48be59c839adf1e1cf917b5b1174b56b525a032b
FreekAlberti <federicoalberi96@gmail.com>
2020-08-25 15:18:16 +0200

    inizio progetto

---
+++ js/script.js
@@ -0,0 +1,3 @@
+//Chiedere all’utente il cognome, inserirlo in un array con altri cognomi, stampa la lista ordinata alfabeticamente, scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];

8ee63e5f4c5ba21776ba9281cc405c1e4215d5ad
FreekAlberti <federicoalberi96@gmail.com>
2020-08-25 15:31:59 +0200

    progetto concluso

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 //Chiedere all’utente il cognome, inserirlo in un array con altri cognomi, stampa la lista ordinata alfabeticamente, scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+//chiedere all'utente il cognome
+
+var cognomeUtente = prompt("Scrivi il tuo cognome");
+
+//inserirlo in array
+
+cognomi.push(cognomeUtente);
+
+//stampa elementi array in ordine alfabetico
+
+cognomi.sort();
+console.log(cognomi);
+
+//scrivi la posizione del cognome inserito
+
+for (var i = 0; i < cognomi.length; i++) {
+  if (cognomi[i] == cognomeUtente) {
+    i++
+    console.log("La posizione del cognome é la numero: " + i)
+  }
+}
